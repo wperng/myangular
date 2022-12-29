3c81c1e496c8b2f6075875cfe44665153e03c62a
wperng <william.perng@gmail.com>
2022-12-29 10:53:12 -0500

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent,  AppHeaderComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
