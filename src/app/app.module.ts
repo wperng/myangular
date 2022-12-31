703e3f9df1e95f8dc8dc1b44e46f95debfd503f2
wperng <william.perng@gmail.com>
2022-12-31 10:10:35 -0500

    6 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,  AppHeaderComponent],
+  imports:      [ BrowserModule, FormsModule, MatToolbarModule, MatIconModule ],
+  declarations: [ AppComponent, AppHeaderComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
